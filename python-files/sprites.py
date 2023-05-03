79c6ece8d0b5d9cbc02a03fef654229544d31faf
Barba2k2 <lorenzo.dz@hotmail.com>
2023-05-03 16:00:40 -0300

    inicio da colisão do player

--- python-files/sprites.py
+++ python-files/sprites.py
@@ -9,6 +9,11 @@ class Generic(pygame.sprite.Sprite):
         self.image = surf
         self.rect = self.image.get_rect(topleft = pos)
 
+class Block(Generic):
+    def __init__(self, pos, size, group):
+        surf = pygame.Surface(size)
+        super().__init__(pos, surf, group)
+
 # simple animated objects  
 class Animated(Generic):
     def __init__(self, assets, pos, group):
@@ -70,28 +75,63 @@ class Shell(Generic):
         self.rect.bottom = self.rect.top + TILE_SIZE
 
 class Player(Generic):
-    def __init__(self, pos, group):
-        super().__init__(pos, pygame.Surface((32, 64)), group)
+    def __init__(self, pos, group, collision_sprites):
+        super().__init__(pos, pygame.Surface((80, 64)), group)
         self.image.fill('blue')
 
         # movement
         self.direction = vector()
-        self.pos = vector(self.rect.topleft)
+        self.pos = vector(self.rect.center)
         self.speed = 300
+        
+        # collision
+        self.collision_sprites = collision_sprites
+        self.hitbox = self.rect.inflate(-50, 0)
         
     def input(self):
         keys = pygame.key.get_pressed()
+        # horizontal movement
         if keys[pygame.K_RIGHT]:
             self.direction.x = 1
         elif keys[pygame.K_LEFT]:
             self.direction.x = -1
         else:
             self.direction.x = 0
+        
+        # vertical movement
+        if keys[pygame.K_DOWN]:
+            self.direction.y = 1
+        elif keys[pygame.K_UP]:
+            self.direction.y = -1
+        else:
+            self.direction.y = 0
     
     def move(self, dt):
-        self.pos += self.direction * self.speed * dt
-        self.rect.topleft = (round(self.pos.x), round(self.pos.y))
-       
+        
+        # horizontal movement
+        self.pos.x += self.direction.x * self.speed * dt
+        self.hitbox.centerx = round(self.pos.x)
+        self.rect.centerx = self.hitbox.centerx
+        self.collision('horizontal')
+        
+        # vertical movement
+        self.pos.y += self.direction.y * self.speed * dt
+        self.hitbox.centery = round(self.pos.y)
+        self.rect.centery = self.hitbox.centery
+        self.collision('vertical')
+    
+    def collision(self, direction):
+        for sprite in self.collision_sprites:
+            if sprite.rect.colliderect(self.hitbox):
+                if direction == 'horizontal':
+                    self.hitbox.right = sprite.rect.left if self.direction.x > 0 else self.hitbox.right #moving right
+                    self.hitbox.left = sprite.rect.right if self.direction.x < 0 else self.hitbox.left #moving left
+                    self.rect.centerx, self.pos.x = self.hitbox.centerx, self.hitbox.centerx
+                else: #vertical
+                    self.hitbox.top = sprite.rect.bottom if self.direction.y < 0 else self.hitbox.top
+                    self.hitbox.bottom = sprite.rect.bottom if self.direction.y > 0 else self.hitbox.bottom
+                    self.rect.centery, self.pos.y = self.hitbox.centery, self.hitbox.centery
+     
     def update(self, dt):
         self.input()
         self.move(dt)
